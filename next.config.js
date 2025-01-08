1849837939eeb2ec99d7c61a27c6f4b031de8528
Your Name <sudeep@kalpas.in>
2025-01-08 13:30:54 +0530

    Build ready

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export', // If you're planning to export static HTML
+//     distDir: 'dist', // Optional: specify custom build directory
+//   };
+  
+//   module.exports = nextConfig;
